eba520880c9298b56c18916e98ce9f193dd3c343
Frederic Kerksieck <frederic.kerksieck@rwth-aachen.de>
2023-07-13 03:19:50 +0200

    added preload script

--- src/background.js
+++ src/background.js
@@ -3,6 +3,8 @@
 import { app, protocol, BrowserWindow } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS3_DEVTOOLS } from "electron-devtools-installer";
+import * as path from "path";
+
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Scheme must be registered before the app is ready
@@ -20,6 +22,7 @@ async function createWindow() {
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
+      preload: path.resolve(__static, 'preload.js'),
     },
   });
 

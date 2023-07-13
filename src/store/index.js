7e7c2c3840dfb216ee1e2d4c643e6f52b58401dc
Frederic Kerksieck <frederic.kerksieck@rwth-aachen.de>
2023-07-13 02:50:48 +0200

    added store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,43 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    nodes: [],
+    edges: [],
+  },
+  getters: {
+    getNodes: (state) => {
+      return state.nodes;
+    },
+    getEdges: (state) => {
+      return state.edges;
+    },
+    getGraph: (getters) => {
+      return {
+        nodes: getters.getNodes,
+        edges: getters.getEdges,
+      };
+    },
+  },
+  mutations: {
+    mutateNodes(state, payload) {
+      state.nodes = payload.nodes;
+    },
+    mutateEdges(state, payload) {
+      state.edges = payload.edges;
+    },
+  },
+  actions: {
+    setNodes({ commit }, payload) {
+      commit("mutateNodes", payload);
+    },
+    setEdges({ commit }, payload) {
+      commit("mutateEdges", payload);
+    },
+    setGraph({ commit }, payload) {
+      commit("mutateNodes", payload);
+      commit("mutateEdges", payload);
+    },
+  },
   modules: {},
 });

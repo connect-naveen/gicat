ada37c7d9352ba9d0dc42f89c505f8b56552aa40
Frederic Kerksieck <frederic.kerksieck@rwth-aachen.de>
2023-07-13 02:28:50 +0200

    added icons

--- src/main.js
+++ src/main.js
@@ -9,7 +9,22 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+// Icons
+import { aliases, mdi } from "vuetify/lib/iconsets/mdi";
+import "@mdi/font/css/materialdesignicons.css";
+
+// create Vuetify
 const vuetify = createVuetify({
+  theme: {
+    defaultTheme: "dark",
+  },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
   components,
   directives,
 });
--- src/views/ExtractorView.vue
+++ src/views/ExtractorView.vue
@@ -1,24 +1,6 @@
 <template>
   <div class="extractor">
     <h1>This is the extractor page</h1>
-    <svg-icon type="mdi" :path="path"></svg-icon>
+    <v-icon icon="mdi-check-circle"></v-icon>
   </div>
 </template>
-
-<script>
-import SvgIcon from "@jamescoyle/vue-icon";
-import { mdiAccount } from "@mdi/js";
-
-export default {
-  name: "ExtractorView",
-  components: {
-    SvgIcon,
-  },
-
-  data() {
-    return {
-      path: mdiAccount,
-    };
-  },
-};
-</script>

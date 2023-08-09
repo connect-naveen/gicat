f959e43ccead1b5a35b093c52c4b16d7948bc243
Frederic Kerksieck <frederic.kerksieck@rwth-aachen.de>
2023-08-09 23:26:26 +0200

    clean preload

--- public/preload.js
+++ public/preload.js
@@ -1,22 +1,2 @@
 const ce = require('./codeextractor/index');
-const { contextBridge } = require('electron')
-
-console.log(ce);
-
-window.testPreload = function() {
-    console.log("preload.js loaded");
-};
-
 window.ce = ce;
-
-// contextBridge.exposeInMainWorld('preloaded', {
-//   ce: () => process.preloaded.ce,
-//   test: () => process.preloaded.testPreload(),
-// });
-
-// contextBridge.exposeInMainWorld('versions', {
-//   node: () => process.versions.node,
-//   chrome: () => process.versions.chrome,
-//   electron: () => process.versions.electron
-//   // we can also expose variables, not just functions
-// })

9298ce9fa044acc2e2af910102c439f2f229ea76
Frederic Kerksieck <frederic.kerksieck@rwth-aachen.de>
2023-08-14 16:51:40 +0200

    testing eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,11 @@ module.exports = {
     parser: "@babel/eslint-parser",
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": "off",
+    "no-debugger": "off",
+    "eqeqeq": "off",
+    // "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
   overrides: [
     {

1e7243ecc6d1420368824aa59cf33c6aa1face47
DavidHe <david.heyen@rwth-aachen.de>
2024-04-02 21:37:05 +0200

    landing page cleanup

--- src/background.js
+++ src/background.js
@@ -34,6 +34,7 @@ async function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
     height: 720,
+    icon: "/logo.png",
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info

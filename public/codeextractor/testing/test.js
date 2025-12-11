5e92b410e92250bb28d2ef2b2dd8c3f2e01fe86e
David H <david.heyen@rwth-aachen.de>
2025-12-11 14:19:38 +0100

    removed some git submodule infos

--- public/codeextractor/testing/test.js
+++
@@ -1,5 +0,0 @@
-const ce = require('../index')
-
-console.log('reading graph...')
-const graph = ce.getGraph("./testing/testfiles")
-console.log(graph)
--- public/codeextractor/testing/testfiles/subclass.js
+++
@@ -1,9 +0,0 @@
-let Shape = require('./class')
-
-class Rectangle extends Shape {
-  constructor(height, width) {
-    // height/width of rectangle
-    this.height = height;
-    this.width = width;
-  }
-}
